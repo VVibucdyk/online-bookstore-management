88ff2c8f7538d67b6db6fe990d25605d33bd5894
Fajar Alam <sidik3998@gmail.com>
2024-04-29 09:32:17 +0700

    wadidaw

--- routes/api.php
+++ routes/api.php
@@ -31,28 +31,29 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('get-user-customer', [UserController::class, 'getUserCustomer'])->name('api.getUserCustomer');
     Route::put('update-role', [UserController::class, 'updateRole'])->name('api.updateUserRole');
     Route::delete('delete-user', [UserController::class, 'deleteUser'])->name('api.deleteUser');
-});
 
-// ENDPOINT API BERHUBUNGAN DENGAN ROLE
-Route::get('get-all-role', [RoleController::class, 'getAllRole'])->name('api.getAllRole');
+    // ENDPOINT API BERHUBUNGAN DENGAN ROLE
+    Route::get('get-all-role', [RoleController::class, 'getAllRole'])->name('api.getAllRole');
+
+    Route::post('create-book', [BookController::class, 'createBook'])->name('api.createBook');
+    Route::put('edit-book', [BookController::class, 'editBook'])->name('api.editBook');
+    Route::delete('delete-book', [BookController::class, 'deleteBook'])->name('api.deleteBook');
+
+    // ENDPOINT API BERHUBUNGAN DENGAN CART
+    Route::get('get-cart/{user_id}', [CartUserController::class, 'getCountUserCart'])->name('api.getCountUserCart');
+    Route::get('get-cart-item/{user_id}', [CartUserController::class, 'getListCartItem'])->name('api.getListCartItem');
+    Route::post('insert-cart/{user_id}/{book_id}', [CartUserController::class, 'insertCart'])->name('api.insertCart');
+    Route::post('decrease-cart/{user_id}/{book_id}', [CartUserController::class, 'decreaseCart'])->name('api.decreaseCart');
+    Route::post('remove-cart/{user_id}/{book_id}', [CartUserController::class, 'removeCart'])->name('api.removeCart');
+
+    // ENDPOINT API BERHUBUNGAN DENGAN ORDER TRANSACTION
+    Route::post('create-order', [OrderTransactionController::class, 'createOrder'])->name('api.createOrder');
+    Route::get('get-list-order/${user_id}', [OrderTransactionController::class, 'getListOrderTransaction'])->name('api.getListOrderTransaction');
+});
 
 // ENDPOINT API BERHUBUNGAN DENGAN BOOK
 Route::get('get-all-book', [BookController::class, 'getAllBooks'])->name('api.getAllBooks');
-Route::post('create-book', [BookController::class, 'createBook'])->name('api.createBook');
-Route::put('edit-book', [BookController::class, 'editBook'])->name('api.editBook');
-Route::delete('delete-book', [BookController::class, 'deleteBook'])->name('api.deleteBook');
 
 
 // ENDPOINT API BERHUBUNGAN DENGAN GENRE
 Route::get('get-all-genres', [GenreController::class, 'getAllGenres'])->name('api.getAllGenres');
-
-// ENDPOINT API BERHUBUNGAN DENGAN CART
-Route::get('get-cart/{user_id}', [CartUserController::class, 'getCountUserCart'])->name('api.getCountUserCart');
-Route::get('get-cart-item/{user_id}', [CartUserController::class, 'getListCartItem'])->name('api.getListCartItem');
-Route::post('insert-cart/{user_id}/{book_id}', [CartUserController::class, 'insertCart'])->name('api.insertCart');
-Route::post('decrease-cart/{user_id}/{book_id}', [CartUserController::class, 'decreaseCart'])->name('api.decreaseCart');
-Route::post('remove-cart/{user_id}/{book_id}', [CartUserController::class, 'removeCart'])->name('api.removeCart');
-
-// ENDPOINT API BERHUBUNGAN DENGAN ORDER TRANSACTION
-Route::post('create-order', [OrderTransactionController::class, 'createOrder'])->name('api.createOrder');
-Route::get('get-list-order/${user_id}', [OrderTransactionController::class, 'getListOrderTransaction'])->name('api.getListOrderTransaction');
